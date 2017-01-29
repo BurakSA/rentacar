606b11bb774c9fa2df13942fd1dffe79c4a6af70
BurakSA <buraksaglm@gmail.com>
2017-01-29 15:10:41 +0300

    Cars controller modified.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,7 @@
 class CarsController < ApplicationController
   def index
-    @aralar = [marka:'Opel', model:'Corsa', Yil:2015]
+    @aralar = [{marka:'Opel', model:'Corsa', Yil:2015},
+               {marka:'Pejo', model:'206', Yil:1998},
+               {marka:'Furki', model:'keko', Yil:2000}]
   end
 end
